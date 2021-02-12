98c90e37a765529962be5018ed4a6648e2506368
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-02-12 13:00:41 -0800

    add changes to app.js

---
+++ resources/js/app.js
@@ -0,0 +1,25 @@
+class HashMap {
+    constructor(length) {
+        this.hashmap = Array(length).fill(null);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        for (let char = 0; char < key.length; char++) {
+            hashCode += hashCode + key.charCodeAt(char);
+        }
+        return hashCode % this.hashmap.length
+    }
+
+
+    assign(key, value) {
+        const hashBucket = this.hash(key);
+        this.hashmap[hashBucket] = value;
+    }
+};
+
+const myHashMap = new HashMap(5);
+myHashMap.assign('Persistence', 'never giving up, even in the face of obstacles');
+myHashMap.assign('Growth Mindset', 'the belief that you can always continue to improve');
+
+console.log(myHashMap.hashmap);
